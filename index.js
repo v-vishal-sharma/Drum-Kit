957b43460a1ba1d124984aa14a743230827aa8c9
v-vishal-sharma <vishals200415@gmail.com>
2021-10-12 10:56:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var drumButtons = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i<drumButtons; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+  var audio = new Audio("sounds/tom-1.mp3");
+  audio.play();
+});
+
+}
